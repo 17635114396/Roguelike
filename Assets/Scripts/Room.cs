edb2d9339cf0ad41a874ffe47a15b83ab5d7692f
17635114396 <2769158060@qq.com>
2020-05-23 16:21:43 +0800

    最远房间优化
    最远步长和次步长里取单门

---
+++ Assets/Scripts/Room.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Room : MonoBehaviour
+{ 
+    public GameObject updoor, downdoor, leftdoor, rightdoor;
+    public bool uproom, downroom, leftroom, rightroom;
+
+    public int stepToStart;
+    public Text stepText;
+
+    public int doorNumber;
+    // Start is called before the first frame update
+    void Start()
+    {
+        updoor.SetActive(uproom);
+        downdoor.SetActive(downroom);
+        leftdoor.SetActive(leftroom);
+        rightdoor.SetActive(rightroom);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void UpDateRoom() {
+        stepToStart = (int)(Mathf.Abs(transform.position.x/66) + Mathf.Abs(transform.position.y/36));
+        stepText.text = stepToStart.ToString();
+        if (uproom)
+            doorNumber++;
+        if (downroom)
+            doorNumber++;
+        if (leftroom)
+            doorNumber++;
+        if (rightroom)
+            doorNumber++;   
+    }
+}
--- Assets/Scripts/Roomgenerater.cs
+++ Assets/Scripts/Roomgenerater.cs
@@ -20,25 +20,29 @@ public class Roomgenerater : MonoBehaviour
     public float Xoffset;
     public float Yoffset;
 
-    public List<GameObject> rooms = new List<GameObject>();
+    public int maxStep;
 
+    public List<Room> rooms = new List<Room>();
+
+    List<GameObject> farRooms = new List<GameObject>();
+    List<GameObject> lessRooms = new List<GameObject>();
+    List<GameObject> oneDoorRooms = new List<GameObject>();
     // Start is called before the first frame update
     void Start()
     {
         for (int i = 0; i < roomNumber; i++)
         {
-            rooms.Add(Instantiate(roomPrefab, StartPoint.position, Quaternion.identity));
+            rooms.Add(Instantiate(roomPrefab, StartPoint.position, Quaternion.identity).GetComponent<Room>());
             ChangePoint();
         }
 
         rooms[0].GetComponent<SpriteRenderer>().color = startColor;
-        endroom = rooms[0];
+
         foreach (var room in rooms)
         {
-            if (room.transform.position.sqrMagnitude>endroom.transform.position.sqrMagnitude) {
-                endroom = room;
-            }
+            SetRoom(room,room.transform.position);
         }
+        FindEndRoom();
         endroom.GetComponent<SpriteRenderer>().color = endColor;
     }
 
@@ -70,5 +74,45 @@ public class Roomgenerater : MonoBehaviour
                     break;
             }
         } while (Physics2D.OverlapCircle(StartPoint.position, 0.2f, roomlayer));
+    }
+
+    public void SetRoom(Room room,Vector3 roomPosition) {
+        room.uproom = Physics2D.OverlapCircle(roomPosition+new Vector3(0,Yoffset,0), 0.2f, roomlayer);
+        room.downroom = Physics2D.OverlapCircle(roomPosition + new Vector3(0,-Yoffset, 0), 0.2f, roomlayer);
+        room.leftroom = Physics2D.OverlapCircle(roomPosition + new Vector3(-Xoffset, 0, 0), 0.2f, roomlayer);
+        room.rightroom = Physics2D.OverlapCircle(roomPosition + new Vector3(Xoffset, 0, 0), 0.2f, roomlayer);
+        room.UpDateRoom();
+    }
+
+    public void FindEndRoom() {
+        for (int i = 0; i < rooms.Count; i++)
+        {
+            if (rooms[i].stepToStart > maxStep)
+                maxStep = rooms[i].stepToStart;
+        }
+
+        foreach (var room in rooms)
+        {
+            if (room.stepToStart == maxStep)
+                farRooms.Add(room.gameObject);
+            if (room.stepToStart == maxStep - 1)
+                lessRooms.Add(room.gameObject);
+        }
+
+        for (int i = 0; i < farRooms.Count; i++)
+        {
+            if (farRooms[i].GetComponent<Room>().doorNumber == 1)
+                oneDoorRooms.Add(farRooms[i]);
+        }
+
+        for (int i = 0; i < lessRooms.Count; i++)
+        {
+            if (lessRooms[i].GetComponent<Room>().doorNumber == 1)
+                oneDoorRooms.Add(lessRooms[i]);
+        }
+        if (oneDoorRooms.Count != 0)
+            endroom = oneDoorRooms[Random.Range(0, oneDoorRooms.Count)];
+        else if(oneDoorRooms.Count == 0)
+            endroom = farRooms[Random.Range(0, oneDoorRooms.Count)]; 
     }
 }
